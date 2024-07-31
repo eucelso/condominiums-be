d45513d61ef994a6511518628d3a820d8db5d024
Celso Alves <luizcelso.fa@gmail.com>
2024-07-31 23:44:08 +0100

    update new version node

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql2',
     connection: {
-      host: env('DATABASE_HOST'),
+      host: env('DATABASE_MYSQL_HOST'),
       port: env.int('DATABASE_PORT'),
       database: env('DATABASE_NAME'),
       user: env('DATABASE_USERNAME'),
