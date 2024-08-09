56afb259d3633b79d992356b8b938fad4c1d12cf
Celso Alves <luizcelso.fa@gmail.com>
2024-08-09 19:48:38 +0100

    update remove some config in our admin

--- src/admin/app.js
+++ src/admin/app.js
@@ -45,22 +45,22 @@ const bootstrap = (app) => {
       location.href = url;
     }
 
-  const styleTag = document.createElement("style");
-      styleTag.innerText = `
-      a[href*="/dashboard/plugins/cloud"], a[href*="/strapi/strapi/releases/tag/v"], a[href*="strapi.io/"], a[href*="cloud.strapi.io"] {
-        display: none;
-      }
-      nav[aria-label="Settings"] ol li:nth-child(2), nav[aria-label="Settings"] ol ol li:nth-child(3) {
-        display: none;
-      }
-      aside[aria-labelledby="join-the-community"], .home-page #main-content>div:first-child>img {
-        display: none;
-      }
-      main > div:nth-child(2) > :nth-child(2) {
-        display: none;
-      }
-    `;
-    document.head.appendChild(styleTag);
+  // const styleTag = document.createElement("style");
+  //     styleTag.innerText = `
+  //     a[href*="/dashboard/plugins/cloud"], a[href*="/strapi/strapi/releases/tag/v"], a[href*="strapi.io/"], a[href*="cloud.strapi.io"] {
+  //       display: none;
+  //     }
+  //     nav[aria-label="Settings"] ol li:nth-child(2), nav[aria-label="Settings"] ol ol li:nth-child(3) {
+  //       display: none;
+  //     }
+  //     aside[aria-labelledby="join-the-community"], .home-page #main-content>div:first-child>img {
+  //       display: none;
+  //     }
+  //     main > div:nth-child(2) > :nth-child(2) {
+  //       display: none;
+  //     }
+  //   `;
+  //   document.head.appendChild(styleTag);
 };
 
 export default {
