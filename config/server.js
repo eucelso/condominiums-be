f88f2254ddce01b73db76ec3a616dd89ead1fa1f
Celso Alves <luizcelso.fa@gmail.com>
2024-08-17 12:46:38 +0100

    remove configs in server.js

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', env('HOST')),
-  port: env.int('PORT', env('PORT')),
-  url: env('PUBLIC_URL', env('PUBLIC_URL')),
+  // host: env('HOST', env('HOST')),
+  // port: env.int('PORT', env('PORT')),
+  // url: env('PUBLIC_URL', env('PUBLIC_URL')),
   app: {
     keys: env.array('APP_KEYS'),
   },
