e2cbda66a04af1f04725fad59d145870ce46f976
Celso Alves <luizcelso.fa@gmail.com>
2024-08-17 11:31:53 +0100

    update config server

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
-  // host: env('HOST', env('APP_HOST')),
-  // port: env.int('PORT', env('APP_PORT')),
-  // url: env('PUBLIC_URL', env('PUBLIC_URL')),
+  host: env('HOST', env('APP_HOST')),
+  port: env.int('PORT', env('APP_PORT')),
+  url: env('PUBLIC_URL', env('PUBLIC_URL')),
   app: {
     keys: env.array('APP_KEYS'),
   },
