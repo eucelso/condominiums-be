6c239a4f0bc47a1a86eeecae07768d2da20a6bcd
Celso Alves <luizcelso.fa@gmail.com>
2024-09-20 00:09:44 +0100

    feat: remove config middleware

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,20 +1,20 @@
 module.exports = [
   'strapi::errors',
-  // 'strapi::security',
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "script-src": ["https://cdnjs.cloudflare.com"],
-          "media-src": ["https://cdnjs.cloudflare.com", "https://cidadevivaoffices.urbanpulse.app.br/"],
-          "img-src": ["https://cdnjs.cloudflare.com", "https://cidadevivaoffices.urbanpulse.app.br/"],
-        },
-      },
-    },
-  },
+  'strapi::security',
+  // {
+  //   name: "strapi::security",
+  //   config: {
+  //     contentSecurityPolicy: {
+  //       useDefaults: true,
+  //       directives: {
+  //         "connect-src": ["'self'", "https:"],
+  //         "script-src": ["https://cdnjs.cloudflare.com"],
+  //         "media-src": ["https://cdnjs.cloudflare.com", "https://cidadevivaoffices.urbanpulse.app.br/"],
+  //         "img-src": ["https://cdnjs.cloudflare.com", "https://cidadevivaoffices.urbanpulse.app.br/"],
+  //       },
+  //     },
+  //   },
+  // },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
